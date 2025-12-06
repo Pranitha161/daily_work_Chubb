3dbc681476f37ec7b10c083001a0e842dad42949
Pranitha <pranithareddy160805@gmail.com>
2025-12-06 19:36:07 +0530

    added jwt-authentication-practice files

---
+++ Dec_6/src/main/java/com/example/demo/entity/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String username;
+    private String password;
+
+}
